e766b81a0c1c65460aced15d1287cedaf76edea8
Juslesan <santeri.juslenius@gmail.com>
2018-05-15 14:40:08 +0300

    refactoring

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import './index.css';
+import {Navbar, NavbarBrand,NavItem, Nav, MenuItem, NavDropdown } from 'react-bootstrap'
+
+class App extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        courses: [],
+      }
+      
+    }
+    render () {
+        return (
+        <div>
+            <Navbar collapseOnSelect>
+                <Navbar.Header>
+                <Navbar.Brand>
+                    Kurssikartta-sovellus
+                </Navbar.Brand>
+                <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                <Nav>
+                    <NavItem href="#">
+                    Kandi
+                    </NavItem>
+                    <NavItem href="#">
+                    Maisteri
+                    </NavItem>
+                    <NavItem href="#">
+                    Linjat
+                    </NavItem>
+                    <NavItem>
+          
+                    </NavItem>
+                </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <img src="https://raw.githubusercontent.com/juhapekkamoilanen/cshy-coursemap/master/cs-hy-coursemap.png" alt="kuva" width="1200"/>
+          </div>
+        )
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,37 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import {Navbar, NavbarBrand,NavItem, Nav, MenuItem, NavDropdown } from 'react-bootstrap'
-
-const App = () => (
-  <div>
-
-  <Navbar collapseOnSelect>
-    <Navbar.Header>
-      <Navbar.Brand>
-        Kurssikartta-sovellus
-      </Navbar.Brand>
-      <Navbar.Toggle />
-    </Navbar.Header>
-    <Navbar.Collapse>
-      <Nav>
-        <NavItem href="#">
-          Kandi
-        </NavItem>
-        <NavItem href="#">
-          Maisteri
-        </NavItem>
-        <NavItem href="#">
-          Linjat
-        </NavItem>
-        <NavItem>
-          
-        </NavItem>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-      <img src="https://raw.githubusercontent.com/juhapekkamoilanen/cshy-coursemap/master/cs-hy-coursemap.png" alt="kuva" width="1200"/>
-  </div>
-)
+import App from './App.js'
 
 ReactDOM.render(<App />, document.getElementById('root'))
