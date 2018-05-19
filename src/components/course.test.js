08e25a68917096d8f4b1e2f51382c3a2c722946f
Juslesan <santeri.juslenius@gmail.com>
2018-05-19 10:23:37 +0300

    Coloring tests

--- src/components/course.test.js
+++ src/components/course.test.js
@@ -68,3 +68,88 @@ describe.only('<Course course={compulsory course}/>', () => {
 
 })
 
+describe.only('Course buttons get coloring based on study track', () => {
+    const softwareSystems = {
+        code: "TKT21010",
+        name: "Full Stack -websovelluskehitys harjoitustyö",
+        level: "Aineopinnot",
+        compulsory: false,
+        prereqs: [],
+        studytrack: ["ss"],
+        ects: "1-10",
+        url: "https://courses.helsinki.fi/fi/TKT21010"
+    }
+    const algorithms = {
+        code: "TKT20001",
+        name: "Tietorakenteet ja algoritmit",
+        level: "Aineopinnot",
+        compulsory: true,
+        prereqs: [],
+        studytrack: ["adm"],
+        ects: "10",
+        url: "https://courses.helsinki.fi/fi/TKT20001"
+    }
+    const networking = {
+        code: "TKT20004",
+        name: "Tietoliikenteen perusteet",
+        level: "Aineopinnot",
+        compulsory: true,
+        prereqs: [],
+        studytrack: ["ns"],
+        ects: "5",
+        url: "https://courses.helsinki.fi/fi/TKT20004"
+
+    }
+    const datascience = {
+        code: "DATA11001",
+        name: "Introduction to Data Science",
+        level: "Syventävät",
+        compulsory: false,
+        prereqs: [],
+        studytrack: ["ds"],
+        ects: "5",
+        url: "https://courses.helsinki.fi/fi/DATA1101"
+
+    }
+    it('software systems', () => {
+        
+        const mockHandler = jest.fn()
+        const courseComponent = shallow(<Course course ={softwareSystems}/>)
+        const contentDiv = courseComponent.find('.noncompulsory')
+        // console.log(courseComponent.html())
+        // console.log(contentDiv.html())
+        // console.log(contentDiv.text())
+        expect(contentDiv.html()).toContain("background-color:yellow")
+    })
+    it('algorithms', () => {
+        
+        const mockHandler = jest.fn()
+        const courseComponent = shallow(<Course course ={algorithms}/>)
+        const contentDiv = courseComponent.find('.compulsory')
+        // console.log(courseComponent.html())
+        // console.log(contentDiv.html())
+        // console.log(contentDiv.text())
+        expect(contentDiv.html()).toContain("background-color:lightblue")
+    })
+    it('networking', () => {
+        
+        const mockHandler = jest.fn()
+        const courseComponent = shallow(<Course course ={networking}/>)
+        const contentDiv = courseComponent.find('.compulsory')
+        // console.log(courseComponent.html())
+        // console.log(contentDiv.html())
+        // console.log(contentDiv.text())
+        expect(contentDiv.html()).toContain("background-color:bisque")
+    })
+    it('data science', () => {
+        
+        const mockHandler = jest.fn()
+        const courseComponent = shallow(<Course course ={datascience}/>)
+        const contentDiv = courseComponent.find('.noncompulsory')
+        // console.log(courseComponent.html())
+        // console.log(contentDiv.html())
+        // console.log(contentDiv.text())
+        expect(contentDiv.html()).toContain("background-color:red")
+    })
+})
+
