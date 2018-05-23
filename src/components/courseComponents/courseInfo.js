26a4137b0642434a8f1bbc1ee135293c7cb9059f
Mikko Moisio <mikko.moisio@helsinki.fi>
2018-05-23 19:57:38 +0300

    kurssitoteutuksien tiedot näkyviin

--- src/components/courseComponents/courseInfo.js
+++ src/components/courseComponents/courseInfo.js
@@ -1,36 +1,91 @@
 import React from 'react'
 import courseInfoService from '../../services/courseinfo'
 // Kurssitietojen renderointi
-const CourseInfo = ({ course }) => {
-    
-    const courseInfo =  courseInfoService.getCourseInfo(course.code)
-    console.log(courseInfo)
-    return (
-        <div>
-            <p style={{ fontWeight: 'bold' }}>{course.name}<br />{course.code} ({course.ects} op)</p>
-            <a href={course.url}>Kurssisivu</a>
-            <br />
-            {course.compulsory ?
-                <p>Pakollinen kurssi</p> :
-                <p>Valinnainen kurssi</p>
-            }
-            <p fontWeight='bold'>Esitiedot:</p>
 
-            {course.prereqs.length !== 0 ?
-                <div className="prereqs" style={{ paddingLeft: 8 }}>
-                    {course.prereqs.map(prereq =>
-                        <p>{prereq}</p>
-                    )}
-                </div> :
-                <div className="noPrereqs" style={{ paddingLeft: 8 }}>
-                    <p>Ei esitietoja</p>
-                </div>
-            }
+
+
+export default class CourseInfo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            course: props.course,
+        }
+    }
+
+    componentDidMount() {
+        courseInfoService.getCourseInfo(this.state.course.code).then(courseInfo => {
+            this.setState({ courseInfo })
+            console.log(this.state.courseInfo)
+        }
+        )
+    }
+
+    render() {
+        return (
             <div>
-                <p>ilmo</p>
-                <p>{courseInfo.ilmoauki}</p>
+                <p style={{ fontWeight: 'bold' }}>{this.state.course.name}<br />{this.state.course.code} ({this.state.course.ects} op)</p>
+                <a href={this.state.course.url}>Kurssisivu</a>
+                <br />
+                {this.state.course.compulsory ?
+                    <p>Pakollinen kurssi</p> :
+                    <p>Valinnainen kurssi</p>
+                }
+                <p fontWeight='bold'>Esitiedot:</p>
+
+                {this.state.course.prereqs.length !== 0 ?
+                    <div className="prereqs" style={{ paddingLeft: 8 }}>
+                        {this.state.course.prereqs.map(prereq =>
+                            <p>{prereq}</p>
+                        )}
+                    </div> :
+                    <div className="noPrereqs" style={{ paddingLeft: 8 }}>
+                        <p>Ei esitietoja</p>
+                    </div>
+                }
+                <div>
+
+                    {this.state.courseInfo !== undefined ?
+                        <div>
+                            <p>Opintojaksot</p>
+                            {this.state.courseInfo.map(opintokohde => {
+                                console.log('opintokohde: ', opintokohde)
+                                return (
+                                    <div>
+                                        {console.log('opintokohteen tunniste: ', opintokohde.opintokohteenTunniste)}
+                                        <div>
+
+                                            <p>{opintokohde.opintokohteenTunniste}</p>
+                                        </div>
+                                        <div>
+                                            {opintokohde.opetustapahtumat.map(opetustapahtuma => {
+                                                console.log('key',opetustapahtuma.key)
+                                                return (
+                                                    <div>
+                                                        
+                                                        <small>
+                                                            <text>
+                                                              Toteutus: {opetustapahtuma.nimi}  \n
+                                                                Tyyppi: {opetustapahtuma.tyyppi}
+                                                                Alkamisaika: {opetustapahtuma.alkamisaika}
+                                                                Loppumisaika: {opetustapahtuma.loppumisaika}
+                                                                Ilmoittautuminen käynnissä: {opetustapahtuma.ilmoittautuminenKaynnissa ? "Kyllä" : "Ei"}
+                                                            </text>
+                                                        </small>
+                                                    </div>
+                                                )
+                                            })}
+                                        </div>
+                                    </div>
+                                )
+                            }
+                            )
+                            }
+                        </div>
+                        : <p></p>
+                    }
+                </div>
+
             </div>
-        </div>
-    )
+        )
+    }
 }
-export default CourseInfo
--- src/services/courseinfo.js
+++ src/services/courseinfo.js
@@ -1,37 +1,12 @@
 import axios from 'axios'
-const baseUrl = 'https://weboodi.helsinki.fi/hy/api/public/opetushaku/hae?nimiTaiTunniste='
+const baseUrl = 'http://localhost:3001/courses/info/'
 
-const mapper = (item) => {
-    return {
-        nimi: item.opetustapahtumanNimi,
-        alkamisaika: item.alkuPvm,
-        loppumisaika: item.loppuPvm,
-        tyyppi: item.opetustapahtumanTyyppiSelite,
-        ilmoittautuminenKaynnissa: (item.tila === "ilmoittautuminen_kaynnissa")
-    }
-}
 
 const getCourseInfo = async (id) => {
-    console.log('url: ', baseUrl.concat(id))
-    return axios
-        .get(baseUrl.concat(id), {
-            headers: {
-                'Access-Control-Allow-Origin': '*'
-            }
-        })
-        .then(response => {
-            return response.data.map((kohde) => {
-                return {
-                    opintokohteenTunniste: kohde.opintokohde.opintokohteenTunniste,
-                    opintokohteenNimi: kohde.opintokohde.opintokohteenNimi,
-                    opetustapahtumat: kohde.opetustapahtumat.map(mapper()),
-                    ilmoauki: kohde.ilmoittautumiskelpoinen
-                }
-            })
-
-        })
-
+    console.log('request uri', baseUrl.concat(id))
+    return await axios.get(baseUrl.concat(id)).then(response => response.data)
 }
+
 
 
 
