900a87dd4b58ac09fb433b7799429c48a829198c
Mikko Moisio <mikko.moisio@helsinki.fi>
2018-05-23 15:20:35 +0300

    avoimet_ilmot

--- src/components/courseComponents/courseInfo.js
+++ src/components/courseComponents/courseInfo.js
@@ -1,12 +1,15 @@
 import React from 'react'
-
+import courseInfoService from '../../services/courseinfo'
 // Kurssitietojen renderointi
-const CourseInfo = ({course}) => {
+const CourseInfo = ({ course }) => {
+    
+    const courseInfo =  courseInfoService.getCourseInfo(course.code)
+    console.log(courseInfo)
     return (
         <div>
-            <p style={{fontWeight: 'bold'}}>{course.name}<br/>{course.code} ({course.ects} op)</p>
+            <p style={{ fontWeight: 'bold' }}>{course.name}<br />{course.code} ({course.ects} op)</p>
             <a href={course.url}>Kurssisivu</a>
-            <br/>
+            <br />
             {course.compulsory ?
                 <p>Pakollinen kurssi</p> :
                 <p>Valinnainen kurssi</p>
@@ -14,15 +17,19 @@ const CourseInfo = ({course}) => {
             <p fontWeight='bold'>Esitiedot:</p>
 
             {course.prereqs.length !== 0 ?
-            <div className="prereqs" style={{paddingLeft:8}}> 
-                {course.prereqs.map(prereq =>
-                    <p>{prereq}</p>
-                )}
-            </div> :
-            <div className="noPrereqs" style={{paddingLeft:8}}>
-                <p>Ei esitietoja</p>
-            </div>
+                <div className="prereqs" style={{ paddingLeft: 8 }}>
+                    {course.prereqs.map(prereq =>
+                        <p>{prereq}</p>
+                    )}
+                </div> :
+                <div className="noPrereqs" style={{ paddingLeft: 8 }}>
+                    <p>Ei esitietoja</p>
+                </div>
             }
+            <div>
+                <p>ilmo</p>
+                <p>{courseInfo.ilmoauki}</p>
+            </div>
         </div>
     )
 }
--- src/services/courseinfo.js
+++ src/services/courseinfo.js
@@ -1,35 +1,35 @@
 import axios from 'axios'
 const baseUrl = 'https://weboodi.helsinki.fi/hy/api/public/opetushaku/hae?nimiTaiTunniste='
 
+const mapper = (item) => {
+    return {
+        nimi: item.opetustapahtumanNimi,
+        alkamisaika: item.alkuPvm,
+        loppumisaika: item.loppuPvm,
+        tyyppi: item.opetustapahtumanTyyppiSelite,
+        ilmoittautuminenKaynnissa: (item.tila === "ilmoittautuminen_kaynnissa")
+    }
+}
 
 const getCourseInfo = async (id) => {
-
-    const request = axios.get(baseUrl.contact(id)).then(response => {
-
-        const array = response.data
-        const opintokohteet = new Array(array.length)
-        for (i = 0; i < opintokohteet.length; i++) {
-          //  console.log("array", array[i])
-            opintokohteet[i] = {
-                opintokohteenTunniste: array[i].opintokohde.opintokohteenTunniste,
-                opintokohteenNimi: array[i].opintokohde.opintokohteenNimi,
-                opetustapahtumat: array[i].opetustapahtumat.map((item) => {
-                    const obj = {
-                        nimi: item.opetustapahtumanNimi,
-                        alkamisaika: item.alkuPvm,
-                        loppumisaika: item.loppuPvm,
-                        tyyppi: item.opetustapahtumanTyyppiSelite,
-                        ilmoittautuminenKaynnissa: (item.tila === "ilmoittautuminen_kaynnissa")
-                    }
-
-                    return obj
-                }
-                )
+    console.log('url: ', baseUrl.concat(id))
+    return axios
+        .get(baseUrl.concat(id), {
+            headers: {
+                'Access-Control-Allow-Origin': '*'
             }
-        }
+        })
+        .then(response => {
+            return response.data.map((kohde) => {
+                return {
+                    opintokohteenTunniste: kohde.opintokohde.opintokohteenTunniste,
+                    opintokohteenNimi: kohde.opintokohde.opintokohteenNimi,
+                    opetustapahtumat: kohde.opetustapahtumat.map(mapper()),
+                    ilmoauki: kohde.ilmoittautumiskelpoinen
+                }
+            })
 
-        return request.then(response => response.data)
-    })
+        })
 
 }
 
