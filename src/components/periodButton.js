11b6422ff819e5ac712b64782379ccba9fbb6704
Tomi R <tomi@1BQQH7U.kumpula.dhcp.hupnet.helsinki.fi>
2018-06-05 15:04:58 +0300

    checkboxit periodeille ja vuosille

--- src/components/periodButton.js
+++ src/components/periodButton.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Popup from 'reactjs-popup'
-import { Button } from 'react-bootstrap'
+import { Button, Checkbox, DropdownButton, MenuItem } from 'react-bootstrap'
 import './periodButton.css'
+
 
 const PeriodButton = (props) => {
 
@@ -16,9 +17,28 @@ const PeriodButton = (props) => {
                 trigger={popupButton()}
                 modal
                 closeOnDocumentClick>
-                <span><Periods period={props.period} callbackFromParent={props.callbackFromParent} /></span>
+                <span>
+                    <Years year={props.year} yearCallback={props.yearCallback} />
+                    <Periods p1={props.p1} p2={props.p2} p3={props.p3}
+                        p4={props.p4} pC={props.pC} pS={props.pS} callback={props.callback}
+                    /></span>
             </Popup>
         </div>
+    )
+}
+
+const Years = (props) => {
+    return (
+        <DropdownButton
+            title={props.year}
+            id='1'
+        >
+            <MenuItem eventKey="2018" name="2018" active={props.year === '2018'} onClick={props.yearCallback}>2018</MenuItem>
+            <MenuItem eventKey="2019" name="2019" active={props.year === '2019'} onClick={props.yearCallback}>2019</MenuItem>
+            <MenuItem eventKey="2020" name="2020" active={props.year === '2020'} onClick={props.yearCallback}>2020</MenuItem>
+            <MenuItem eventKey="2021" name="2021" active={props.year === '2021'} onClick={props.yearCallback}>2021</MenuItem>
+            <MenuItem eventKey="2022" name="2022" active={props.year === '2022'} onClick={props.yearCallback}>2022</MenuItem>
+        </DropdownButton>
     )
 }
 
@@ -28,16 +48,16 @@ const Periods = (props) => {
     function klikHandle(arvo) {
         props.callbackFromParent(arvo)
     }*/
-    
+
     return (
         <div>
-            <Button onClick={() => props.callbackFromParent('one')}>Periodi 1</Button>
-            <Button onClick={() => props.callbackFromParent('two')}>Periodi 2</Button>
-            <Button onClick={() => props.callbackFromParent('three')}>Periodi 3</Button>
-            <Button onClick={() => props.callbackFromParent('four')}>Periodi 4</Button>
-            <Button onClick={() => props.callbackFromParent('christmas')}>Joulu</Button>
-            <Button onClick={() => props.callbackFromParent('summer')}>Kesä</Button>
-            <Button onClick={() => props.callbackFromParent('all')}>Kaikki</Button>
+
+            <Checkbox checked={props.p1} onChange={props.callback} name="p1">Periodi 1</Checkbox>
+            <Checkbox checked={props.p2} onChange={props.callback} name='p2'>Periodi 2</Checkbox>
+            <Checkbox checked={props.p3} onChange={props.callback} name='p3'>Periodi 3</Checkbox>
+            <Checkbox checked={props.p4} onChange={props.callback} name='p4'>Periodi 4</Checkbox>
+            <Checkbox checked={props.pC} onChange={props.callback} name='pC'>Joulu</Checkbox>
+            <Checkbox checked={props.pS} onChange={props.callback} name='pS'>Kesä</Checkbox>
         </div>
     )
 }
