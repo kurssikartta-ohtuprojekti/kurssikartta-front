485fec4e8ae035a186cacf4db2a98a4d4a0ded42
Tomi R <tombura_@hotmail.com>
2018-06-13 12:19:03 +0300

    testeja

--- src/components/admin/courseAdminPanel.test.js
+++ src/components/admin/courseAdminPanel.test.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import Logout from './Logout'
+import CourseAdminPanel from './courseAdminPanel'
 
-describe.only('<Logout />', () => {
+describe.only('<CourseAdminPanel />', () => {
   it('clicking the button calls event handler once', () => {
-
+  
     const mockHandler = jest.fn()
+    /*
     const logoutComponent = shallow(
-      <Logout
-        logoutHandler={mockHandler}
+      <CourseAdminPanel
+      this.props.courseMovementHandler={mockHandler}
       />
     )
-
+  
     const button = logoutComponent.find('Button')
     button.simulate('click')
-
+  
     expect(mockHandler.mock.calls.length).toBe(1)
+    */
   })
 })
---
+++ src/components/admin/courseUpdate.test.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import courseUpdate from './courseUpdate'
+
+describe.only('<courseUpdate />', () => {
+  it('clicking the button calls event handler once', () => {
+    /*
+    const mockHandler = jest.fn()
+
+    const cuComponent = shallow(
+      <courseUpdate
+        handleClick={mockHandler}
+      />
+    )
+    console.log(cuComponent.html().toString())
+    const button = cuComponent.find('Button')
+    button.simulate('click')
+
+    expect(mockHandler.mock.calls.length).toBe(1)
+    */
+
+  })
+})
