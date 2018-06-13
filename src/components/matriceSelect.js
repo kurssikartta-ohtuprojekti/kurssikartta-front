93bf5e14d7c1982507e57b9242b840f1b312d0a6
mvarilo <miro.varilo@helsinki.fi>
2018-06-13 10:52:15 +0300

    Backend url ja console logeja pois

--- src/components/matriceSelect.js
+++ src/components/matriceSelect.js
@@ -11,9 +11,9 @@ const MatriceSelect = (props) => {
 }
 
 const Kartta = (props) => {
-    console.log(props.matrices)
-    console.log(props.selected)
-    console.log("selected")
+    // console.log(props.matrices)
+    // console.log(props.selected)
+    // console.log("selected")
 
     var matrices = props.matrices;
 
@@ -26,13 +26,11 @@ const Kartta = (props) => {
                 <null></null> :
                 props.matrices.map(matrice =>
                     <div>
-                        {console.log(matrice)}
+                        {/* {console.log(matrice)} */}
                         <MenuItem name={matrice.id} active={props.selected === matrice.id} onClick={props.matriceCallback}>{matrice.name}</MenuItem>
                     </div>
                 )
             }
-            {/* <MenuItem eventKey="0" name={names[0]} active={props.selected === names[0]} onClick={props.matriceCallback}>{names[0]}</MenuItem>
-            <MenuItem eventKey="1" name={names[1]} active={props.selected === names[1]} onClick={props.matriceCallback}>{names[1]}</MenuItem> */}
         </DropdownButton>
     )
 }
