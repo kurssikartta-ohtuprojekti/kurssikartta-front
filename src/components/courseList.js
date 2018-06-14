48b88bf756bb3e7e54f44f43ce21e986d233a200
mvarilo <miro.varilo@helsinki.fi>
2018-06-14 12:23:20 +0300

    course list in rows

--- src/components/courseList.js
+++ src/components/courseList.js
@@ -12,31 +12,31 @@ const CourseList = ({ perus, aine, syv, mat, stats }) => (
         <div className="perus" style={{ float: 'left', padding: 4 }}>
             <h2>Perusopinnot</h2>
             {perus.map(course =>
-                <Course key={course.code} course={course} />
+                <Course key={course.code} course={course} style={{ display: 'inline-block' }} />
             )}
         </div>
         <div className="aine" style={{ float: 'left', padding: 4 }}>
             <h2>Aineopinnot</h2>
             {aine.map(course =>
-                <Course key={course.code} course={course} style={{}} />
+                <Course key={course.code} course={course} style={{ display: 'inline-block' }} />
             )}
         </div>
         <div className="syventavat" style={{ float: 'left', padding: 4 }}>
             <h2>Syventävät opinnot</h2>
             {syv.map(course =>
-                <Course key={course.code} course={course} />
+                <Course key={course.code} course={course} style={{ display: 'inline-block' }} />
             )}
         </div>
         <div className="matematiikka" style={{ float: 'left', padding: 4 }}>
             <h2>Matematiikan opinnot</h2>
             {mat.map(course =>
-                <Course key={course.code} course={course} />
+                <Course key={course.code} course={course} style={{ display: 'inline-block' }}/>
             )}
         </div>
         <div className="tilastotiede" style={{ float: 'left', padding: 4 }}>
             <h2>Tilastotieteen opinnot</h2>
             {stats.map(course =>
-                <Course key={course.code} course={course} />
+                <Course key={course.code} course={course} style={{ display: 'inline-block' }} />
             )}
         </div>
     </div>
