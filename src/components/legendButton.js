c9c56d5ca6425acf4b974bfc2f7a323b033edf5d
Tomi R <tombura_@hotmail.com>
2018-06-19 21:18:38 +0300

    legend update

--- src/components/legendButton.js
+++ src/components/legendButton.js
@@ -26,12 +26,13 @@ const Legend = () => {
       <h4>Map Legend:</h4>
       <p>Kurssi vahvana = opetusta valitussa periodissa</p>
       <p>Kurssi himmeänä = ei opetusta valitulla periodilla</p>
-      <p><img src={require('../images/bold.png')} alt="bold" height="10" width="42" /> = Pakollinen kurssi</p>
+      <p><img src={require('../images/bolded.png')} alt="bold" height="21" width="42" /> = Pakollinen kurssi</p>
       <p><img src={require('../images/sin.png')} alt="blue" height="21" width="42" /> = Networking and Services</p>
       <p><img src={require('../images/kel.png')} alt="yellow" height="21" width="42" /> = Software Systems</p>
       <p><img src={require('../images/pun.png')} alt="red" height="21" width="42" /> = Algorithms, Data-analytics and Machine Learning</p>
       <p><img src={require('../images/vih.png')} alt="green" height="21" width="42" /> = Datascience</p>
-      <p><img src={require('../images/orans.png')} alt="orangeish" height="21" width="42" /> = Mathematics and Statistics</p>
+      <p><img src={require('../images/orans.png')} alt="orangeish" height="21" width="42" /> = Mathematics</p>
+      <p><img src={require('../images/pink.png')} alt="pink" height="21" width="42" /> = Statistics</p>
     </div>
   )
 }
