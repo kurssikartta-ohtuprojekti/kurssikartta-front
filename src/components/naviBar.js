02ed7ec6500a76d1f16cd31db73e26d91e3bf7c0
Tomi R <tombura_@hotmail.com>
2018-06-21 10:20:52 +0300

    navbar hover

--- src/components/naviBar.js
+++ src/components/naviBar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Navbar, NavItem, Nav } from 'react-bootstrap'
+import './naviBar.css'
 
 // Renders the navigation bar
 const NaviBar = () => (
@@ -10,22 +11,22 @@ const NaviBar = () => (
           height="51" width="auto" src={require("../images/tko_logo.jpg")} alt="Tko-äly ry"
           style={{padding: 4}}/></a>
             <Navbar.Brand>
-                <b style={{color: 'black'}}>Kurssikartta-sovellus</b>
+                <b style={{color: 'black', }}>Kurssikartta-sovellus</b>
             </Navbar.Brand>
             <Navbar.Toggle />
           </Navbar.Header>
           <Navbar.Collapse>
             <Nav>
                 <NavItem href="/">
-                  <b style={{color: 'black'}}>Etusivu</b>
+                  <b>Etusivu</b>
                 </NavItem>
                 <NavItem href="/kartta">
-                  <b style={{color: 'black'}}>Kartta</b>
+                  <b>Kartta</b>
                 </NavItem>
             </Nav>
             <Nav pullRight>
                 <NavItem href="/admin">
-                  <b style={{color: 'black'}}>Admin view</b>
+                  <b>Admin view</b>
                 </NavItem>
             </Nav>
           </Navbar.Collapse>
