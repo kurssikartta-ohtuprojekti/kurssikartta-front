774b969ce292d1e9968dfdcb1104e06127270298
mvarilo <miro.varilo@helsinki.fi>
2018-06-27 12:23:41 +0300

    fix

--- src/services/register.js
+++ src/services/register.js
@@ -3,7 +3,7 @@ const baseUrl = 'https://kurssikartta-backend.herokuapp.com/register'
 // const baseUrl = 'http://localhost:3001/register'
 
 const register = async (credentials) => {
-    const request = axios.put(`${baseUrl}`, credentials)
+    const request = axios.post(`${baseUrl}`, credentials)
     return await request.then(response => response.data)
 }
 
