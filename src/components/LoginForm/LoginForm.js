0ce47d64f8d156f7c7795a45a7e97d949ea5b10c
mvarilo <miro.varilo@helsinki.fi>
2018-06-27 12:19:16 +0300

    register form final

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ControlLabel, Button, FormGroup, FormControl } from 'react-bootstrap'
-import Reaptcha from 'reaptcha';
+// import Reaptcha from 'reaptcha';
 
 import './LoginForm.css'
 const LoginForm = ({ handleSubmit, loginMessage, handleChange, username, password, reCaptcha, reCaptchaExpire, verified }) => {
@@ -44,14 +44,16 @@ const LoginForm = ({ handleSubmit, loginMessage, handleChange, username, passwor
           // onExpire={reCaptchaExpire}
           /> */}
 
-          <Button className="loginSubmit" bsStyle="success" disabled={!verified} onClick={handleSubmit} id='login'>Login</Button>
-          <Button className="registerSubmit" href="/register" >Register</Button>
+          <Button className="loginSubmit" type="submit" bsStyle="success" disabled={!verified} onClick={handleSubmit} id='login'>Login</Button>
 
         </FormGroup>
       </form>
 
       <div className="message">
         {loginMessage}
+      </div>
+      <div className="registerText">
+        Uusi käyttäjä? <a href="/register">Register</a>
       </div>
     </div>
   )
