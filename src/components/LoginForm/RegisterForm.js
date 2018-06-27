5a2e8fe3274040b3a46681f149dffad24f75eff6
mvarilo <miro.varilo@helsinki.fi>
2018-06-27 13:28:19 +0300

    register suomeksi ja ohjeet

--- src/components/LoginForm/RegisterForm.js
+++ src/components/LoginForm/RegisterForm.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ControlLabel, Button, FormGroup, FormControl } from 'react-bootstrap'
+import { ControlLabel, Button, FormGroup, FormControl, HelpBlock } from 'react-bootstrap'
 import Reaptcha from 'reaptcha';
 
 import './RegisterForm.css'
@@ -7,27 +7,41 @@ const RegisterForm = ({ handleSubmit, loginMessage, handleChange, username, pass
 
     // const reset = () => this.captcha.reset();
 
-    // verified = true;
+    verified = true;
 
     if (verified === false && this.captcha !== null && this.captcha !== undefined) {
         this.captcha.reset();
     }
 
+    // const popoverTop = (
+    //     <Popover id="popover-positioned-scrolling-top" title="Popover top">
+    //         <strong>Popover testi</strong>
+    //     </Popover>
+    // );
+
+
     return (
         <div className="loginWrapper">
-            <h2 className="loginHeader">Register</h2>
+            <h2 className="loginHeader">Rekisteröidy</h2>
 
             <form onSubmit={handleSubmit}>
                 <FormGroup className="registerForm">
-                    <ControlLabel>Username: </ControlLabel>
+                    <ControlLabel>Käyttäjätunnus: </ControlLabel>
+                    {/* <OverlayTrigger
+                        container={this}
+                        trigger="click"
+                        placement="top"
+                        overlay={popoverTop}
+                    > */}
                     <FormControl
                         className="loginInput"
                         value={username}
                         onChange={handleChange}
                         name="username"
                     />
-
-                    <ControlLabel>Password: </ControlLabel>
+                    <HelpBlock>Käyttäjätunnuksen tulee olla 3-10 merkkiä pitkä.</HelpBlock>
+                    {/* </OverlayTrigger> */}
+                    <ControlLabel>Salasana: </ControlLabel>
                     <FormControl
                         className="loginInput"
                         type="password"
@@ -36,14 +50,14 @@ const RegisterForm = ({ handleSubmit, loginMessage, handleChange, username, pass
                         onChange={handleChange}
 
                     />
-
+                    <HelpBlock>Salasanan tulee olla 6-20 merkkiä ja sisältää vähintään yhden numeron.</HelpBlock>
                     <Reaptcha className="captcha"
                         ref={e => (this.captcha = e)}
                         sitekey="6LcE2l8UAAAAALMqws8reJ6Okpv2IT6Z3DhVS8US"
                         onVerify={reCaptcha}
                     // onExpire={reCaptchaExpire}
                     />
-                    <Button className="registerSubmit" disabled={!verified} onClick={handleSubmit} id='register'>Register</Button>
+                    <Button className="registerSubmit" disabled={!verified} onClick={handleSubmit} id='register'>Rekisteröidy</Button>
 
                 </FormGroup>
             </form>
